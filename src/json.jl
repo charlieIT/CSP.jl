1b689bd0fd8b0acb4452ee1e6f4a3817fb349360
charlieIT <>
2023-02-15 23:11:06 +0000

    updated json implementation to reflect policy changes

--- src/json.jl
+++ src/json.jl
@@ -1,11 +1,15 @@
 using JSON3
 
 function JSON3.write(policy::Policy, args...; kwargs...)
-    return JSON3.write(http(Dict(policy)))
+    return JSON3.write(Dict(policy))
 end
 
 function JSON3.write(fname::String, policy::Policy, args...; kwargs...)
     open(fname, "w") do io
         JSON3.pretty(io, JSON3.write(policy, args...; kwargs...))
     end
+end
+
+function Base.show(io::IO, policy::Policy)
+    JSON3.pretty(io, policy)
 end
