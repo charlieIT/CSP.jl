e7f18659953f9771c1baedd04c44c35ad4e8084c
charlieIT <>
2023-02-15 23:10:20 +0000

    update and fix utils methods

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,17 @@
-function _to_prop_name(directive::String)
-    return replace(directive, "-"=>"_")
+function _to_prop_name(directive::String)::String
+    return string(replace(directive, "-"=>"_"))
 end
 _to_prop_name(directive::Symbol) = _to_prop_name(string(directive))
 
-function _directive_name(prop::String)
-    return replace(prop, "_"=>"-")
+function _directive_name(prop::String)::String
+    return string(replace(prop, "_"=>"-"))
 end
 _directive_name(prop::Symbol) = _directive_name(string(prop))
+
+function get_directive(key, default::String=string(key))::String
+    key = _directive_name(string(key))
+    if key in DIRECTIVES
+        return key
+    end
+    return default
+end
